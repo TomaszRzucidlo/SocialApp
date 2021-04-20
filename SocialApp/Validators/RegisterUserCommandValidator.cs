969aa062b7d548bf3ca59e4ff5f5c80a2ad81f5b
TomaszRzucidlo <rzucidlo.tomasz@gmail.com>
2021-04-20 20:23:35 +0200

    Task FluentValidator

---
+++ SocialApp/Validators/RegisterUserCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using SocialApp.INFRASTRUCTURE.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.Validators
+{
+    public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterUserCommandValidator()
+        {
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Password).MinimumLength(8);
+            RuleFor(x => x.FirstName).NotNull().MinimumLength(1);
+            RuleFor(x => x.LastName).NotNull().MinimumLength(1);
+        }
+    }
+}
